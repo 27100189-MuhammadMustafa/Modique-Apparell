c0ebfff8959e43b5f2f1afa3bc8ccd78c0b99fb1
27100189-MuhammadMustafa <javedmustafa557@gmail.com>
2025-07-10 17:30:26 +0500

    fourth commit

---
+++ models/discounts.js
@@ -0,0 +1,33 @@
+const mongoose= require('mongoose');
+const discountSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required:true,
+    },
+    productId: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    }],
+    salePrice: {
+        type: Number,
+        required: true
+    },
+    discountPercentage: {
+        type: Number,
+        required: true
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+});
+module.exports = mongoose.model('Discount', discountSchema);
